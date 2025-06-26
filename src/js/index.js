8ece6a4af7d695fd64cad806b59b2c4ef2ce36a0
Artem Potorochin <sqdteffy@gmail.com>
2025-06-26 03:01:05 +0300

    Half of functionality done

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,61 @@
 import '../scss/style.scss';
+
+const input = document.querySelector('#search');
+const list = document.querySelector('.search-form__list');
+
+async function getRepos(url) {
+  const resp = await fetch(url);
+  const data = await resp.json();
+  const repos = [];
+  if (data.items.length >= 5) {
+    for (let i = 0; i < 5; i++) {
+      const obj = {};
+      obj.name = data.items[i].name;
+      obj.owner = data.items[i].owner.login;
+      obj.stars = data.items[i].stargazers_count;
+      repos.push(obj);
+    }
+  } else if (data.items.length > 0) {
+    for (let i = 0; i < data.items.length; i++) {
+      const obj = {};
+      obj.name = data.items[i].name;
+      obj.owner = data.items[i].owner.login;
+      obj.stars = data.items[i].stargazers_count;
+      repos.push(obj);
+    }
+  }
+  return repos;
+}
+
+const debounce = (cb, time) => {
+    let timeout = undefined;
+    return function() {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => cb.apply(this, arguments), time);
+    }
+}
+
+async function setList() {
+  if (document.querySelectorAll('.search-form__item').length !== 0) {
+    const values = document.querySelectorAll('.search-form__item');
+    for (let item of values) {
+      item.remove();
+    }
+  }
+  if (/[a-zA-Zа-яА-Я]/.test(input.value.trim())) {
+    const req = `https://api.github.com/search/repositories?q=${input.value}`;
+    const items = await getRepos(req);
+    if (items.length !== 0) {
+      for (let info of items) {
+        const item = document.createElement('li');
+        item.classList.add('search-form__item');
+        item.textContent = info.name;
+        list.append(item);
+      }
+    }
+  }
+}
+
+const setListDn = debounce(setList, 500);
+
+input.addEventListener('input', setListDn);
